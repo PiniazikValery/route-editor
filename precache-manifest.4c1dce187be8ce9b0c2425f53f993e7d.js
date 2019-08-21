8130eac8132339428a912e9330972fd8eff01666
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-08-21 17:09:39 +0300

    Updates

--- precache-manifest.4c1dce187be8ce9b0c2425f53f993e7d.js
+++ precache-manifest.4c1dce187be8ce9b0c2425f53f993e7d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a9c49b8951a66e55d57e7e605af586ec",
+    "revision": "f8107e3e67066a54de8d1902380d7d5d",
     "url": "/route-editor/index.html"
   },
   {
-    "revision": "fb10ea639a2208e89270",
+    "revision": "46ff979d20e6804007db",
     "url": "/route-editor/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "cc7848daf5d36614cc95",
-    "url": "/route-editor/static/js/2.3debb0af.chunk.js"
+    "revision": "61c5c6e291f1f24a492d",
+    "url": "/route-editor/static/js/2.65126242.chunk.js"
   },
   {
-    "revision": "fb10ea639a2208e89270",
-    "url": "/route-editor/static/js/main.84b7d6d5.chunk.js"
+    "revision": "46ff979d20e6804007db",
+    "url": "/route-editor/static/js/main.b26603af.chunk.js"
   },
   {
     "revision": "aa8af2bb5b0e56177743",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/route-editor/precache-manifest.07b3b5b104c5f37f1900e7c1b0540918.js"
+  "/route-editor/precache-manifest.4c1dce187be8ce9b0c2425f53f993e7d.js"
 );
 
 self.addEventListener('message', (event) => {
